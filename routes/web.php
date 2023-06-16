622da0d070b01fd8c74c5e0cf68e161dd4d5db3a
Khalid Chowdhury <Khalidchowdhury90@gmail.com>
2023-06-16 20:14:47 +0600

    Slice

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+// Dashboard Route 
+Route::get('home', function(){
+    return view('Dashboard.home');
 });
+
+
+
+// Frontend Route
+
+
+
+
+
+
+
+
+
+
+
+
+
+
